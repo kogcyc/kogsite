71fc0e53714385de97fc0854592c2cb4bcc22f5b
Matthew <kogcyc@gmail.com>
2023-11-25 15:11:08 -0800

    created SITE MAP / FAQ

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+      // Your code here will run after the page is fully loaded
+
+
+
+
+
+
+	// Add event listener to the hamburger image
+	document.getElementById('hamburgerBtn').addEventListener('click', function() {
+	// Navigate to "menu.html" when the hamburger is clicked
+	window.location.href = 'menu.html';
+	});
+
+
+	// Add event listener to the logo image
+	document.getElementById('logoBtn').addEventListener('click', function() {
+	// Navigate to "index.html" when the logo is clicked
+	window.location.href = 'index.html';
+	});
+
+
+
+    });
