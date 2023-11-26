6d1ded308a2d7a1c7253668aa0eee06cd54ddb94
Matthew <kogcyc@gmail.com>
2023-11-26 12:02:13 -0800

    added footer

--- js/script.js
+++ js/script.js
@@ -1,26 +1,17 @@
 
-
+// The code under this will run after the page is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
-      // Your code here will run after the page is fully loaded
+  
+  // Add event listener to the logo image
+  document.getElementById('logoBtn').addEventListener('click', function() {
+    // Navigate to "index.html" when the logo is clicked
+    window.location.href = 'index.html';
+  });
+  
+  // Add event listener to the hamburger image
+  document.getElementById('hamburgerBtn').addEventListener('click', function() {
+    // Navigate to "menu.html" when the hamburger is clicked
+    window.location.href = 'menu.html';
+  });
 
-
-
-
-
-
-	// Add event listener to the hamburger image
-	document.getElementById('hamburgerBtn').addEventListener('click', function() {
-	// Navigate to "menu.html" when the hamburger is clicked
-	window.location.href = 'menu.html';
-	});
-
-
-	// Add event listener to the logo image
-	document.getElementById('logoBtn').addEventListener('click', function() {
-	// Navigate to "index.html" when the logo is clicked
-	window.location.href = 'index.html';
-	});
-
-
-
-    });
+});
